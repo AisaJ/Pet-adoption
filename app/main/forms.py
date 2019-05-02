1bbd3ce4289875a4eb1b2139ccb167857dc5b4ee
amiinegal <you@example.com>
2019-05-02 09:47:10 +0300

    create a message form

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,3 +5,9 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class MessageForm(FlaskForm):
+
+    title = StringField('title',validators=[Required()])
+    message = TextAreaField('message')
+    submit = SubmitField('Submit')
