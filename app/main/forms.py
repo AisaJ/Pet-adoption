f55f2ca1b0196a71626579e41c1122d524cc5cf7
amiinegal <you@example.com>
2019-05-02 12:12:55 +0300

    added a another for a pet

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,3 +11,13 @@ class MessageForm(FlaskForm):
     title = StringField('title',validators=[Required()])
     message = TextAreaField('message')
     submit = SubmitField('Submit')
+
+class PetForm(FlaskForm):
+    name = StringField('Pets Name')  
+    Gender = StringField('Pets Gender', choices=[('Male', 'Male'), ('Female', 'Female')])
+    Age = StringField('Pets Age')
+    description = TextAreaField('Pet Description')
+    Contact = StringField('')
+    submit = SubmitField('Submit')
+    
+    
