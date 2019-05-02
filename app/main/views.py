386fc50c862c2f5040f391fd137394fe6907b509
rehemafaith <rehemafaith01@gmail.com>
2019-05-02 15:54:20 +0300

    added initial page

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,15 @@
 from . import main
-from flask import render_template
-
+from flask import render_template,request,redirect,url_for
 
 @main.route('/')
 def index():
 
   title='This works'
   return render_template('index.html',title=title)
+
+@main.route('/petcare')
+def petcare():
+
+
+  title= "Pet Care"
+  return render_template('petcare.html',title = title)
